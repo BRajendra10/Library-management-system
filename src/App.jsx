8d389142cbde2c4c792647f69392f620383b67e3
Uniquesoul14 <8391soumyanayak@gmail.com>
2025-08-28 11:00:49 +0530

    Updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,39 @@
-import './App'
-import React, { useEffect } from 'react'
-import { useSelector, useDispatch } from 'react-redux'
-import { fetachedBooksData } from './features/BookSlice'
-import { fetchedMembersData } from './features/MemberSlice'
-import { fetchOverdueData } from './features/overdueSlice'
-import { loginUsersData } from './features/LoginSlice'
-import Dashboard from './screens/Dashboard'
-import { setIsLogedIn } from './features/LoginSlice'
+import "./App";
+import React, { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { fetachedBooksData } from "./features/BookSlice";
+import { fetchedMembersData } from "./features/MemberSlice";
+import { fetchOverdueData } from "./features/overdueSlice";
+import { loginUsersData } from "./features/LoginSlice";
+import Dashboard from "./screens/Dashboard";
+import { setIsLogedIn } from "./features/LoginSlice";
+import { fetachedRequestBooksData } from "./features/RequestBookSlice";
 
 function App() {
-  const dispatch = useDispatch()
+  const dispatch = useDispatch();
   const { login } = useSelector((state) => state.login);
+  const { requestbooks } = useSelector((state) => state.requestbooks);
 
   useEffect(() => {
-    dispatch(fetachedBooksData())
-    dispatch(fetchedMembersData())
-    dispatch(fetchOverdueData())
-    dispatch(loginUsersData())
+    dispatch(fetachedBooksData());
+    dispatch(fetchedMembersData());
+    dispatch(fetchOverdueData());
+    dispatch(loginUsersData());
+  }, [dispatch]);
+
+  useEffect(() => {
+    dispatch(fetachedRequestBooksData());
   }, [dispatch]);
 
   useEffect(() => {
     dispatch(setIsLogedIn(login[0]?.isLogedIn));
-  }, [dispatch, login])
+  }, [dispatch, login]);
 
   return (
     <div>
       <Dashboard />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/screens/Overview.jsx
+++ src/screens/Overview.jsx
@@ -1,64 +1,78 @@
-import React, { useEffect } from 'react'
-import Card from '../Components/Card'
-import OverdueCard from '../components/OverdueCard';
-import RequestCard from '../components/RequestCard';
-import { useSelector, useDispatch } from 'react-redux'
-import { setTotalFine } from '../features/overdueSlice'
+import React, { useEffect } from "react";
+import Card from "../Components/Card";
+import OverdueCard from "../components/OverdueCard";
+import RequestCard from "../components/RequestCard";
+import { useSelector, useDispatch } from "react-redux";
+import { setTotalFine } from "../features/overdueSlice";
 
 import { FaBook, FaRegClock } from "react-icons/fa";
 import { FiUsers } from "react-icons/fi";
 import { MdArrowForwardIos } from "react-icons/md";
 import { FaHandHolding } from "react-icons/fa6";
-import { Navigate, useNavigate } from 'react-router-dom';
-import Books from './Books';
+import { Navigate, useNavigate } from "react-router-dom";
+import BookRequestCard from "../components/RequestCard";
+import Books from "./Books";
 
 function Overview() {
   const navigate = useNavigate();
   const dispatch = useDispatch();
-  const { books } = useSelector((state) => state.books)
-  const { members } = useSelector((state) => state.members)
-  const overDueBooks = useSelector((state) => state.overDue.overdueBooks)
-  const Fine = useSelector((state) => state.overDue.Fine)
+  const { books } = useSelector((state) => state.books);
+  const { members } = useSelector((state) => state.members);
+  const requestbooks = useSelector((state) => state.requestbooks.requestbooks);
+  const overDueBooks = useSelector((state) => state.overDue.overdueBooks);
+  const Fine = useSelector((state) => state.overDue.Fine);
+
+    console.log(requestbooks);
 
   useEffect(() => {
-    dispatch(setTotalFine(overDueBooks))
-  }, [overDueBooks, dispatch])
+    dispatch(setTotalFine(overDueBooks));
+  }, [overDueBooks, dispatch]);
 
   return (
     <div className="w-full h-fit grid grid-cols-12">
-
       {/* card one container */}
       <div className="col-span-12 h-[20rem] p-5 overflow-x-auto">
         <div className="flex gap-5 min-w-max xl:grid xl:grid-cols-3 xl:gap-5 xl:overflow-x-visible">
-
           {/* Card 1 */}
           <div className="min-w-[25rem]">
-            <Card booksNum={books.length} info={"borrowed"} num1={"+12"} num2={"+5%"}>
+            <Card
+              booksNum={books.length}
+              info={"borrowed"}
+              num1={"+12"}
+              num2={"+5%"}
+            >
               <FaBook className="text-4xl text-blue-500" />
             </Card>
           </div>
 
           {/* Card 2 */}
           <div className="min-w-[25rem]">
-            <Card booksNum={overDueBooks.length} info={"books overdue"} num1={"-2%"} num2={`₹ ${Fine}`} >
+            <Card
+              booksNum={overDueBooks.length}
+              info={"books overdue"}
+              num1={"-2%"}
+              num2={`₹ ${Fine}`}
+            >
               <FaRegClock className="text-4xl text-blue-500" />
             </Card>
           </div>
 
           {/* Card 3 */}
           <div className="min-w-[25rem]">
-            <Card booksNum={members.length} info={"visitors"} num1={"+42"} num2={"+102%"} >
+            <Card
+              booksNum={members.length}
+              info={"visitors"}
+              num1={"+42"}
+              num2={"+102%"}
+            >
               <FiUsers className="text-4xl text-blue-500" />
             </Card>
           </div>
-
         </div>
       </div>
 
-
       {/* bootom container with two section */}
       <div className="col-span-12 h-full grid grid-cols-12 gap-5 px-5">
-
         <section className="col-span-6 h-[35rem] flex flex-col border border-blue-200 rounded-lg">
           {/* overvue books header */}
           <div className="w-full h-[4rem] flex justify-between items-center px-5">
@@ -74,7 +88,9 @@ function Overview() {
 
           {/* details */}
           <div className="w-full h-full overflow-scroll">
-            {overDueBooks.map((el, inedx) => <OverdueCard id={inedx} data={el} />)}
+            {overDueBooks.map((el, inedx) => (
+              <OverdueCard id={inedx} data={el} />
+            ))}
           </div>
         </section>
 
@@ -94,12 +110,12 @@ function Overview() {
 
           {/* details */}
           <div className="w-full h-full overflow-scroll">
-            <RequestCard />
+          {requestbooks.map((el) => <BookRequestCard data={el} />)}
           </div>
         </section>
       </div>
     </div>
-  )
+  );
 }
 
-export default Overview
+export default Overview;
