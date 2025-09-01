6e297ebe28a2667d9b3a91ae05946cfab5e72859
BRajendra10 <rajendrabehera8116@gmail.com>
2025-09-01 06:56:11 +0530

    showing borrowed Books data on dom

--- src/components/BorrowedBooks.jsx
+++ src/components/BorrowedBooks.jsx
@@ -1,8 +1,30 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { useSelector } from 'react-redux'
+import Book from './Book'
 
 function BorrowedBooks() {
+  const { books } = useSelector((state) => state.books)
+  const { borrowedBooks } = useSelector((state) => state.borrowedBooks);
+  const [bookData, setBookData] = useState([]);
+
+  useEffect(() => {
+    const data = books.filter((book) => 
+      borrowedBooks.find((el) => {
+        if(el.bookId == book.id) return el;
+      })
+    )
+
+    setBookData(data);
+  }, [books, borrowedBooks])
+
   return (
-    <div>BorrowedBooks</div>
+    <div className="w-full h-fit flex flex-col gap-1">
+      {bookData?.length ? (
+        bookData.map((el) => <Book data={el} />)
+      ) : (
+        <p className="p-2 text-stone-500">No Books found.</p>
+      )}
+    </div>
   )
 }
 
